52a5399359ccf51c22008738582031b2d9596169
bhendaaragao <bendaaragao@gmail.com>
2023-09-21 16:15:11 -0300

    segunda edição nos espaçamentos

--- calculadoraDePartidasRanqueadas.js
+++ calculadoraDePartidasRanqueadas.js
@@ -13,17 +13,23 @@ function saldoDeRanqueadas(vitorias, derrotas) {
 function nivelJogador(rank) {
     if (rank <= 10) {
         console.log('O Herói tem saldo de', rank, 'e está no nível de Ferro');
-    } else if (rank >= 11 && rank <= 20) {
+    } 
+    else if (rank >= 11 && rank <= 20) {
         console.log('O Herói tem saldo de', rank, 'e está no nível de Bronze');
-    } else if (rank >= 21 && rank <= 50) {
+    } 
+    else if (rank >= 21 && rank <= 50) {
         console.log('O Herói tem saldo de', rank, 'e está no nível de Prata');
-    } else if (rank >= 51 && rank <= 80) {
+    } 
+    else if (rank >= 51 && rank <= 80) {
         console.log('O Herói tem saldo de', rank, 'e está no nível de Ouro');
-    } else if (rank >= 81 && rank <= 90) {
+    } 
+    else if (rank >= 81 && rank <= 90) {
         console.log('O Herói tem saldo de', rank, 'e está no nível de Diamante');
-    } else if (rank >= 91 && rank <= 100) {
+    } 
+    else if (rank >= 91 && rank <= 100) {
         console.log('O Herói tem saldo de', rank, 'e está no nível de Lendário');
-    } else {
+    } 
+    else {
         console.log('O Herói tem saldo de', rank, 'e está no nível de Imortal');
     }
 }
